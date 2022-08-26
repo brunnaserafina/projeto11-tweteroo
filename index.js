44496aa56dc2d8cfd4ec7d01a61ec937f2f14ac4
Brunna Serafina <brunnaserafina@gmail.com>
2022-08-25 23:33:11 -0300

    feat: receiving parameters (username and avatar), saving in the api and returning message 'OK' in the path '/sign-up'

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const users = [];
+
+server.post("/sign-up", (req, res) => {
+	const user = req.body;
+
+	users.push(user);
+
+	res.send("OK");
+});
+
+server.listen(5000);
