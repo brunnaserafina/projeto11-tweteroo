b9ac0784cc7d1572b977a3d363cf13bb2c8268a1
Brunna Serafina <brunnaserafina@gmail.com>
2022-08-25 23:40:49 -0300

    feat: receiving parameters (username and tweet), saving in the api and returning message 'OK' in the path '/tweets'

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ server.use(express.json());
 server.use(cors());
 
 const users = [];
+const tweets = [];
 
 server.post("/sign-up", (req, res) => {
 	const user = req.body;
 
 	users.push(user);
+
+	res.send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+	const tweet = req.body;
+	const picture = users.find((value) => value.username === tweet.username);
+	const avatar = picture.avatar;
+
+	tweet.avatar = avatar;
+	tweets.push(tweet);
 
 	res.send("OK");
 });
